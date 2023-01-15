bd6d4ce0b321d9d769f512b832080a7504641ee0
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2023-01-15 23:11:25 +0530

    Currency Conversion Implementation Started

--- src/components/CurrencyConverter.js
+++ src/components/CurrencyConverter.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 
 export default function CurrencyConverter() {
   const [currencySymbols, setCurrencySymbols] = useState([]);
+  const [selectorOne, setSelectorOne] = useState("USD");
+  const [selectorTwo, setSelectorTwo] = useState("INR");
   useEffect(() => {
     const getSymbols = async () => {
       // Define Headers
@@ -17,17 +19,63 @@ export default function CurrencyConverter() {
         requestOptions
       );
       const data = await response.json();
+      // console.log(Object.keys(data.symbols));
+      setCurrencySymbols(Object.keys(data.symbols));
+    };
+    getSymbols();
+
+    const convertCurrency = async (to, from, amount) => {
+      let myHeaders = new Headers();
+      myHeaders.append("apikey", "dznzx2HwMzCohBpZq2JXpPyldLRWUT5L");
+      const requestOptions = {
+        method: "GET",
+        redirect: "follow",
+        headers: myHeaders
+      };
+      const response = await fetch(
+        `https://api.apilayer.com/exchangerates_data/convert?to=${to}&from=${from}&amount=${amount}`,
+        requestOptions
+      );
+      const data = await response.json();
       console.log(data);
     };
+
+    convertCurrency("USD", "INR", "80");
   }, []);
+
+  const handleSelectorOne = (e) => {
+    setSelectorOne(e.target.value);
+  };
+  const handleSelectorTwo = (e) => {
+    setSelectorTwo(e.target.value);
+  };
 
   return (
     <div className="center">
       <h1>Currency Converter</h1>
-      <select></select>
+      <select value={selectorOne} onChange={handleSelectorOne}>
+        {currencySymbols.map((element, i) => {
+          return (
+            <option key={i} value={element}>
+              {element}
+            </option>
+          );
+        })}
+        {/* <option value="volvo">Volvo</option> */}
+      </select>
+
       <input type="text" id="currencyOne" className="currency1" />
       <br />
-      <select></select>
+      <select value={selectorTwo} onChange={handleSelectorTwo}>
+        {currencySymbols.map((element, i) => {
+          return (
+            <option key={i} value={element}>
+              {element}
+            </option>
+          );
+        })}
+        {/* <option value="volvo">Volvo</option> */}
+      </select>
       <input type="text" id="currencyTwo" className="currency2" />
     </div>
   );
