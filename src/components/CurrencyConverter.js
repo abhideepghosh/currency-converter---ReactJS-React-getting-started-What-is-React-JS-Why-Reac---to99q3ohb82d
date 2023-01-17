d256571f504ec82346a6e51c9c6c10f2bd347fde
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2023-01-17 21:13:33 +0530

    Update CurrencyConverter.js

--- src/components/CurrencyConverter.js
+++ src/components/CurrencyConverter.js
@@ -1,107 +1,110 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function CurrencyConverter() {
-  const [currencySymbols, setCurrencySymbols] = useState([]);
-  const [selectorOne, setSelectorOne] = useState("USD");
-  const [selectorTwo, setSelectorTwo] = useState("INR");
-  const [currencyFrom, setCurrencyFrom] = useState("");
-  const [currencyTo, setCurrencyTo] = useState("");
+  let currencies = [
+    "CAD",
+    "HKD",
+    "CZK",
+    "INR",
+    "CHF",
+    "EUR",
+    "JPY",
+    "USD",
+    "AUD",
+  ];
+  const [baseCurrency, setBaseCurrency] = useState("USD");
+  const [otherCurrency, setotherCurrency] = useState("INR");
+  const [baseValue, setbaseValue] = useState(0);
+  const [conversionRate, setConversionRate] = useState(0);
+  const [convertedValue, setConvertedValue] = useState(0);
 
   useEffect(() => {
-    const getSymbols = async () => {
-      // Define Headers
-      let myHeaders = new Headers();
-      myHeaders.append("apikey", "dznzx2HwMzCohBpZq2JXpPyldLRWUT5L");
-      const requestOptions = {
-        method: "GET",
-        redirect: "follow",
-        headers: myHeaders
-      };
-      const response = await fetch(
-        "https://api.apilayer.com/exchangerates_data/symbols",
-        requestOptions
-      );
-      const data = await response.json();
-      // console.log(Object.keys(data.symbols));
-      setCurrencySymbols(Object.keys(data.symbols));
-    };
-    getSymbols();
-  }, []);
-
-  const convertCurrency = async (to, from, amount) => {
-    let myHeaders = new Headers();
-    myHeaders.append("apikey", "dznzx2HwMzCohBpZq2JXpPyldLRWUT5L");
-    const requestOptions = {
-      method: "GET",
-      redirect: "follow",
-      headers: myHeaders
-    };
-    const response = await fetch(
-      `https://api.apilayer.com/exchangerates_data/convert?to=${to}&from=${from}&amount=${amount}`,
-      requestOptions
-    );
-    const data = await response.json();
-    console.log(data);
-  };
-
-  /* ------------Inputs------------ */
-  const handleSelectorOne = (e) => {
-    setSelectorOne(e.target.value);
-  };
-  const handleSelectorTwo = (e) => {
-    setSelectorTwo(e.target.value);
-  };
-  const handleFrom = (e) => {
-    setCurrencyFrom(e.target.value);
-  };
-
-  const handleTo = (e) => {
-    setCurrencyTo(e.target.value);
-  };
+    fetch(`https://api.exchangeratesapi.io/latest?base=${baseCurrency}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setConversionRate(data.rates[otherCurrency]);
+      });
+  }, [baseCurrency]);
 
   return (
-    <div className="center">
-      <h1>Currency Converter</h1>
-      <select value={selectorOne} onChange={handleSelectorOne}>
-        {currencySymbols.map((element, i) => {
-          return (
-            <option key={i} value={element}>
-              {element}
-            </option>
-          );
-        })}
-        {/* <option value="volvo">Volvo</option> */}
-      </select>
-
-      <input
-        type="text"
-        id="currencyOne"
-        value={currencyFrom}
-        onChange={handleFrom}
-        className="currency1"
-      />
-      <br />
-      <select value={selectorTwo} onChange={handleSelectorTwo}>
-        {currencySymbols.map((element, i) => {
-          return (
-            <option key={i} value={element}>
-              {element}
-            </option>
-          );
-        })}
-        {/* <option value="volvo">Volvo</option> */}
-      </select>
-      <input
-        type="text"
-        id="currencyTwo"
-        value={currencyTo}
-        onChange={handleTo}
-        className="currency2"
-      />
+    <div className="row mt-5">
+      {" "}
+      <div className="col-md-4 offset-md-4 border p-5">
+        <h1>Currency Converter</h1>
+        <form>
+          <div className="form-row mt-5">
+            <div className="col-3">
+              <select
+                className="form-control baseCurrency "
+                onChange={(e) => {
+                  setBaseCurrency(e.target.value);
+                }}
+                name="baseCurrency"
+                value={baseCurrency}
+              >
+                {currencies.map((currency) => (
+                  <option key={currency} value={currency}>
+                    {currency}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div className="col-9">
+              <input
+                className="form-control"
+                name="baseCurrency"
+                type="number"
+                value={baseValue}
+                onChange={(e) => {
+                  if (+e.target.value >= 0) {
+                    setbaseValue(e.target.value);
+                    setConvertedValue(
+                      Math.round(e.target.value * conversionRate * 100) / 100
+                    );
+                  }
+                }}
+              ></input>
+            </div>
+          </div>
+          <div className="form-row mt-2">
+            <div className="col-3">
+              <select
+                className="form-control otherCurrency"
+                onChange={(e) => {
+                  setotherCurrency(e.target.value);
+                }}
+                name="otherCurrency"
+                value={otherCurrency}
+              >
+                {currencies.map((currency) =>
+                  baseCurrency !== currency ? (
+                    <option key={currency} value={currency}>
+                      {currency}
+                    </option>
+                  ) : undefined
+                )}
+              </select>
+            </div>
+            <div className="col-9">
+              <input
+                name="baseCurrency"
+                className="form-control"
+                type="number"
+                onChange={(e) => {
+                  if (+e.target.value >= 0) {
+                    setConvertedValue(e.target.value);
+                    setbaseValue(
+                      Math.round((e.target.value / conversionRate) * 100) / 100
+                    );
+                  }
+                }}
+                value={convertedValue}
+              ></input>
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
-
-//khuda jaane...
-// piya ore piya (atif aslam)
-// all songs of kk
